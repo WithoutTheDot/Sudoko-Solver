4422ee80853a30e816e75b43f4f46c139e4e3c48
WithoutTheDot <shieldsjoshua2402@gmail.com>
2023-08-24 12:09:46 +0100

    user-input

--- main.py
+++ main.py
@@ -2,37 +2,37 @@
 
 
 
-def is_valid_move(grid, row, col, num):
-    for x in range(9):
+def is_valid_move(grid, row, col, num, size):
+    for x in range(size):
         if grid[row][x] == num:
             return False
-    for y in range(9):
+    for y in range(size):
         if grid[y][col] == num:
             return False
     
-    corner_row = row-row % 3
-    corner_col = col - col % 3
-    for x in range(3):
-        for y in range(3):
+    corner_row = row-row % int(size**(1/2))
+    corner_col = col - col % int(size**(1/2))
+    for x in range(int(size**(1/2))):
+        for y in range(int(size**(1/2))):
             if grid[corner_row+x][corner_col+y] == num:
                 return False
             
     return True
 
-def solve(grid, row, col):
-    if col == 9:
-        if row == 8:
+def solve(grid, row, col, size):
+    if col == size:
+        if row == size-1:
             return True
         row += 1
         col = 0
 
     if grid[row][col] > 0:
-        return solve(grid, row, col+1)
+        return solve(grid, row, col+1, size)
     
-    for num in range(1,10):
-        if is_valid_move(grid, row, col, num):
+    for num in range(1,size):
+        if is_valid_move(grid, row, col, num, size):
             grid[row][col] = num
-            if solve(grid,row,col+1):
+            if solve(grid,row,col+1, size):
                 return True
             
         grid[row][col] = 0
@@ -45,20 +45,21 @@ Usage:
         To use this enter the size of the sudoko 
         then the numbers with 0 being empty from left to right and downwards 
         (like reading a book)""")
+    size = int(input("Size of the sudoko: "))
     grid = []
-    for i in range(9):
+    for i in range(size):
         line = []
-        for j in range(9):
-            line.append(int(input(f"Enter row {j} col {i}: ")))
+        for j in range(size):
+            line.append(int(input(f"Enter col {i} row {j}: ")))
         grid.append(line)
-    return grid
+    return grid, size
 
-grid =  get_user_grid() #Enter soduko here
+grid, size =  get_user_grid() #Enter soduko here
 
 
-if solve(grid, 0,0):
-    for i in range(9):
-        for j in range(9):
+if solve(grid, 0,0, size):
+    for i in range(size):
+        for j in range(size):
             print(grid[i][j], end = "  ")
         print()
 else:
